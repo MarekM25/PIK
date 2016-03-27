be5a38c910cde58e8ad7e5d442a48f3bd9ae3434
kstanisz <staniszewski.kacper@gmail.com>
2016-03-27 21:51:39 +0200

    Initial commit. Spring Boot project

---
+++ src/main/java/com/databasemanager/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.databasemanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/databasemanager")
+    public String home(){
+        return "home";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,11 @@
+<%@page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8"%>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Home Page</title>
+</head>
+<body>
+Hello World!
+</body>
+</html>
