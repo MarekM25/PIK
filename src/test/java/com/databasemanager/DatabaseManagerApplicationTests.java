0ba880bd179ad9623f7037dbc2ea216080bb3779
MarekM25 <marekm2504@gmail.com>
2016-03-29 22:30:39 +0200

    Add hello unit test

--- src/test/java/com/databasemanager/DatabaseManagerApplicationTests.java
+++ src/test/java/com/databasemanager/DatabaseManagerApplicationTests.java
@@ -1,11 +1,14 @@
 package com.databasemanager;
 
 import com.databasemanager.configuration.DatabaseManagerApplication;
+import com.databasemanager.controller.HomeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DatabaseManagerApplication.class)
@@ -16,4 +19,11 @@ public class DatabaseManagerApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void helloTest() {
+		HomeController homeController = new HomeController();
+		String expected = "home";
+		String actual = homeController.home();
+		assertEquals("HomeController niepoprawna wartość",expected, actual);
+	}
 }
