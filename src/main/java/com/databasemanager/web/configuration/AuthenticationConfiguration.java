680ec24297f4fcb4d9df9a911de2f1ccc95f5358
kstanisz <staniszewski.kacper@gmail.com>
2016-04-21 23:39:56 +0200

    #PIK-14 Added SpringSecurity configuration, Authentication Configuration

---
+++ src/main/java/com/databasemanager/web/configuration/AuthenticationConfiguration.java
@@ -0,0 +1,19 @@
+package com.databasemanager.web.configuration;
+
+import com.databasemanager.domain.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+
+@Configuration
+public class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(accountService);
+    }
+}
