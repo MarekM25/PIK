7c2a95729c09c01224cb5bb2d7848bcc8dcca6d2
MarekM25 <marekm2504@gmail.com>
2016-04-22 11:55:50 +0200

    #PIK-16 In Progress

--- src/main/java/com/databasemanager/web/Application.java
+++ src/main/java/com/databasemanager/web/Application.java
@@ -10,7 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.databasemanager.domain.model")
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(Application.class, args);
     }
 }
