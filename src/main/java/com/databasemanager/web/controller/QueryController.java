42c1cfbf99ec6327bef7860cb7f70334a887efc0
kstanisz <staniszewski.kacper@gmail.com>
2016-06-06 00:12:48 +0200

    #PIK-53 In Progress Rest Controller

--- src/main/java/com/databasemanager/web/controller/QueryController.java
+++ src/main/java/com/databasemanager/web/controller/QueryController.java
@@ -2,13 +2,13 @@ package com.databasemanager.web.controller;
 
 import com.databasemanager.domain.service.QueryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/query")
 public class QueryController {
 
@@ -16,18 +16,19 @@ public class QueryController {
     private QueryService queryService;
 
     @RequestMapping(value = "/select")
-    public String executeQuery() {
-        try{
-            List<String[]> results=queryService.executeSelectQuery("select * from account");
-            for(String[] row:results){
-                for(String tuple :row){
-                    System.out.print(tuple +" | ");
+    public List<String[]> executeQuery() {
+        try {
+            List<String[]> results = queryService.executeSelectQuery("select * from account");
+            for (String[] row : results) {
+                for (String tuple : row) {
+                    System.out.print(tuple + " | ");
                 }
                 System.out.println();
             }
-        }catch(SQLException e){
+            return results;
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        return "account/welcome";
+        return null;
     }
 }
