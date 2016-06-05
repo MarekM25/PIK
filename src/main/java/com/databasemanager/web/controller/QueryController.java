cc9bf82b98bfacf03eb33e3428d7052f185cfcb8
kstanisz <staniszewski.kacper@gmail.com>
2016-06-06 00:14:27 +0200

    #PIK-53 In Progress RestContoller

--- src/main/java/com/databasemanager/web/controller/QueryController.java
+++ src/main/java/com/databasemanager/web/controller/QueryController.java
@@ -18,7 +18,7 @@ public class QueryController {
     @RequestMapping(value = "/select")
     public List<String[]> executeQuery() {
         try {
-            List<String[]> results = queryService.executeSelectQuery("select * from account");
+            List<String[]> results= queryService.executeSelectQuery("select * from account");
             for (String[] row : results) {
                 for (String tuple : row) {
                     System.out.print(tuple + " | ");
