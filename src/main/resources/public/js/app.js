f9da8d348928aefe2280ed015bd38aa7360b4585
MarekM25 <marekm2504@gmail.com>
2016-06-06 01:27:38 +0200

    Change method to post #PIK 53 InProgress

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -11,12 +11,12 @@ app.controller("AppCtrl",function($scope,$http) {
     $scope.execute= function () {
         $http({
             url: '../query/select',
-            method: "GET",
+            method: "POST",
             data: { 'message' : $scope.queryText }
         })
             .then(function(response) {
-                    console.log('sukces');
-                    console.log(response);
+                    $scope.queryResult = response.data;
+                    console.log($scope.queryResult);
                 },
                 function(response) {
                     console.log('lipa');
