f11f7a7490e4297a6907d7e55f765c57f55bd76d
pawello2222 <pawello2222@gmail.com>
2016-06-06 01:55:09 -0700

    Added Connection model and repository

---
+++ src/main/java/com/databasemanager/domain/repository/ConnectionRepository.java
@@ -0,0 +1,12 @@
+package com.databasemanager.domain.repository;
+
+import com.databasemanager.domain.model.Connection;
+
+
+@NoRepositoryBean
+public interface ConnectionRepository extends RepositoryBase<Connection, Long>
+{
+    Connection findByUsername(String username);
+}
+
+
