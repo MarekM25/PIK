23e082e830cbc8a215510d69184c0eef55f63525
pawello2222 <pawello2222@gmail.com>
2016-06-06 02:26:36 -0700

    Added connection service

---
+++ src/main/java/com/databasemanager/domain/service/ConnectionServiceImpl.java
@@ -0,0 +1,41 @@
+package com.databasemanager.domain.service;
+
+import com.databasemanager.domain.dto.ConnectionDTO;
+//import com.databasemanager.domain.exception.UsernameNotAvailableException;
+import com.databasemanager.domain.model.Connection;
+import com.databasemanager.domain.repository.ConnectionRepository;
+
+import java.util.List;
+
+@Service
+public class ConnectionServiceImpl extends EntityServiceBase<Connection, ConnectionDTO> implements ConnectionService
+{
+
+    private ConnectionRepository connectionRepository;
+
+    @Autowired
+    public ConnectionServiceImpl(ConnectionRepository connectionRepository) {
+        super(Connection.class, ConnectionDTO.class);
+        this.connectionRepository = connectionRepository;
+    }
+
+
+    @Override
+    @Transactional
+    public ConnectionDTO createConnection(ConnectionDTO connectionDTO)
+    {
+        Connection connection = super.convertToEntity(connectionDTO);
+        connection = connectionRepository.save(connection);
+        return this.convertToDTO(connection);
+    }
+
+
+    @Override
+    @Transactional
+    public void deleteConnection(long id)
+    {
+        //TODO
+    }
+}
+
+
